cbb8f04a15052f9fccdaafb5ec063f116638acda
ArceusOmkar7 <68581782+ArceusOmkar7@users.noreply.github.com>
2022-10-18 18:41:47 +0530

    deleted class

--- TC Form Generator/main.py
+++ TC Form Generator/main.py
@@ -1,22 +1,18 @@
 import pandas as pd
 
-class FormGen:
-    def __init__(self, data_location:str):
-        print("INITIALIZED")
-        self.excel_data = pd.read_csv(data_location)
-        self.adm_nos = [x for x in self.excel_data['Admission Number']]
+
+excel_data = pd.read_csv("student data.csv")
+adm_nos = [x for x in excel_data['Admission Number']]
 
 
-    def check_student(self):
-        adm_no = int(input("Enter Admission Number of student: "))
+def check_student():
+    adm_no = int(input("Enter Admission Number of student: "))
 
-        if adm_no in self.adm_nos:
-            print("Student enrolled")
-            return True
-        elif adm_no not in self.adm_nos:
-            print("Student not enrolled")
-            return False
+    if adm_no in adm_nos:
+        print("Student enrolled")
+        return True
+    elif adm_no not in adm_nos:
+        print("Student not enrolled")
+        return False
 
-app = FormGen(data_location = "student data.csv")
-
-app.check_student()
+check_student()
