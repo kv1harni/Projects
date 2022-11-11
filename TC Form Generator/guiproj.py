8466d1ac4cde6afbdb4f7dfb87df60c4abbe9678
Aayush Mishra <95838151+iamAayushMishra@users.noreply.github.com>
2022-11-11 22:50:13 +0530

    added scrollbar

--- TC Form Generator/guiproj.py
+++ TC Form Generator/guiproj.py
@@ -2,16 +2,20 @@ import tkinter as tk
 from tkinter import *
 from tkinter import messagebox, ttk, filedialog 
 import pandas as pd
-
+import numpy 
 
 root = Tk()
 
     
 def readExcel(fileName):
     return ""
+
+def GenerateTcForm():
+    print(tree.item(tree.focus()))
+
+
 def OpenFile():
-    filename = filedialog.askopenfile(title="Open Student Details Excel File", filetypes=[("Excel files", "*.xlsx; *.xls")])
-    
+    filename = filedialog.askopenfilename(title="Open Student Details Excel File", filetypes=[("xlxs files", ".*xlsx"), ("All Files", "*.")])
     if filename: 
         try:
             filename = r"{}".format(filename)
@@ -20,15 +24,47 @@ def OpenFile():
         except:
             messagebox.showerror("Error", "You can\'t access this file type")
 
-    tree.delete()
+        
 
+    tree.delete(*tree.get_children())
+    
+    # treeScroll = Scrollbar(frame)
+    # treeScroll2 = Scrollbar(frame, orient="horizontal")
+
+    # treeScroll.pack(side=RIGHT, fill=Y)
+    # treeScroll2.pack(side=BOTTOM, fill='x')
+    # tree.pack()
+
+    # treeScroll.config(command=tree.yview)
+    # treeScroll2.config(command=tree.xview)
+    # tree.config(yscrollcommand= treeScroll.set)
+    # tree.config(xscrollcommand= treeScroll2.set)
+
+    
+
+    
     tree['column'] = list(df.columns)
     tree['show'] = "headings"
+
+    #defining heading title
+
+    for col in tree["column"]:
+        tree.heading(col, text=col)
+    
+    #getting the data through numpy
+    df_rows = df.to_numpy().tolist()
+    for row in df_rows:
+        tree.insert("", "end", values=row)
+
+    #button
+    btn2 = Button(root, text="Generate Tc", width=30, height=2, font=20, fg = "white", bg="#0078d7", command=GenerateTcForm)
+    btn2.pack(padx=10, pady=20)
+
 
 
 
 root.title("Transfer form Generator")
-root.geometry("1100x400+200+200")
+root.geometry("1100x550+200+200")
 
 #icon==========================================
 logophoto = tk.PhotoImage(file = 'logo.png')
@@ -41,8 +77,17 @@ frame.pack(pady=25)
 #treeView
 
 tree = ttk.Treeview(frame)
+treeScroll = Scrollbar(frame)
+treeScroll2 = Scrollbar(frame, orient="horizontal")
+
+treeScroll.pack(side=RIGHT, fill=Y)
+treeScroll2.pack(side=BOTTOM, fill='x')
 tree.pack()
 
+treeScroll.config(command=tree.yview)
+treeScroll2.config(command=tree.xview)
+tree.config(yscrollcommand= treeScroll.set)
+tree.config(xscrollcommand= treeScroll2.set)
 
 #button
 btn = Button(root, text="Open", width=60, height=2, font=30, fg = "white", bg="#0078d7", command=OpenFile)
